1873c828690da75e1fffb8c9b90b064f27447e9a
nigeldkat <nigeldkat@hotmail.com>
2023-05-30 15:43:49 -0400

    wire up dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,6 +43,9 @@ import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
 
+
+import { DashboardService } from './components/dashboard/dashboard.service';
+
 // Auth service
 import { AuthService } from "./shared/services/auth.service";
 
@@ -81,7 +84,7 @@ import { AuthService } from "./shared/services/auth.service";
     AngularFireStorageModule,
     AngularFireDatabaseModule,
   ],
-  providers: [AuthService],
+  providers: [AuthService, AngularFirestoreModule, DashboardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
